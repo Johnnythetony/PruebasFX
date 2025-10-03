70c2436be5e9211b2263236acf379bc8fbfd7a8f
Manuel Rico <manuelricolopez@liceolapaz.net>
2025-10-03 13:07:36 +0200

    Pendiente cargar vista welcome

---
+++ src/main/java/edu/rico/javafx/login/WelcomeView.java
@@ -0,0 +1,17 @@
+package edu.rico.javafx.login;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public  class WelcomeView implements Initializable {
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
